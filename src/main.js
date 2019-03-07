265851f30a1de48fe0799011f6125754e8a90a03
shuchen2 <793762488@qq.com>
2019-03-07 09:32:39 +0800

    添加字体vue字体图标

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入vue-awesome
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/index.js'
+
+// 全局注册
+Vue.component('icon', Icon)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
